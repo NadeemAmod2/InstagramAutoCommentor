df513fe6a093b6e929c773d9b098ae27a2d659f5
NadeemAmod2 <55990900+NadeemAmod2@users.noreply.github.com>
2020-05-07 04:47:13 +0800

    Add files via upload

---
+++ autocommentor.py
@@ -0,0 +1,11 @@
+# imports
+from instapy import InstaPy
+from instapy import smart_run
+from instapy import set_workspace
+
+session = InstaPy(username='quarantinecurls', password='s8361423', headless_browser=True)
+
+with smart_run(session):
+	session.set_dont_include(["quarantinecurls"])	# ignore himself
+	session.set_ignore_users(["quarantinecurls"])	# ignore himself
+	session.like_by_tags(["quarantinecurls"], amount=30)
